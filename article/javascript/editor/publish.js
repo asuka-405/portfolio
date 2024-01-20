34216eabab3474141aebc38973ca43ac3ad39ca9
Suryansh <97630173+asuka-405@users.noreply.github.com>
2024-01-20 14:57:55 +0530

    removing garbage

--- article/javascript/editor/publish.js
+++ article/javascript/editor/publish.js
@@ -1,36 +1,33 @@
 import { getCurrentArticleHtml } from "/article/javascript/editor/helpers.js"
 
-export function publishArticle(uname, token, repo) {
+export async function publishArticle(uname, token, repo) {
     const contents = getCurrentArticleHtml()
 
     const title = RegExp(/<title>(.*)<\/title>/).exec(contents)[1]
 
-    uploadToGithub(uname, token, repo, `article/${title}.html`, contents)
+    await uploadToGithub(uname, token, repo, `article/${title}.html`, contents)
+
     updateArchive(title, uname, token, repo)
 }
 
 export function updateArchive(title, uname, token, repo) {
     fetch(
-        `https://api.github.com/repos/${uname}/${repo}/contents/article/index.html`
+        `https://api.github.com/repos/${uname}/${repo}/contents/article/index.html`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: "application/vnd.github.v3+json",
+            },
+        }
     )
         .then((data) => data.json())
-        .then(() => {
-            const dom = new DOMParser().parseFromString(
-                xhr.responseText,
-                "text/html"
-            )
+        .then((domString) => {
+            const dom = new DOMParser().parseFromString(domString, "text/html")
             const newContent = addArticle(dom, title)
-            //updateRepo(uname, token, repo, newContent)
-            console.log(newContent)
+            updateRepo(uname, token, repo, newContent)
         })
 }
-
-updateArchive(
-    null,
-    "asuka-405",
-    "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",
-    "portfolio"
-)
 
 function addArticle(dom, title) {
     const article = dom.createElement("div")
@@ -48,7 +45,7 @@ function updateRepo(uname, token, repo, content) {
     uploadToGithub(uname, token, repo, "article/index.html", content)
 }
 
-function uploadToGithub(uname, token, repo, filePath, content) {
+async function uploadToGithub(uname, token, repo, filePath, content) {
     const url = `https://api.github.com/repos/${uname}/${repo}/contents/${filePath}`
 
     content = btoa(content)
